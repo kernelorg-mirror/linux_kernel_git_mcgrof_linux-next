c5ef40cb32a92957480f9121f33496fd175d2165
Luis R. Rodriguez <mcgrof@kernel.org>
2015-12-08 15:24:58 -0800

    tables.h: add linker table support
    A linker table is a data structure that is stitched together from items
    in multiple object files. Linux has historically implicitly used linker
    tables for ages, however they were all built in an adhoc manner which
    requires linker script modifications, per architecture. This adds a
    general linker table solution so that a new linker table can be
    implemented by changing C code only. The Linux linker table was
    originally based on Michael Brown's iPXE's linker table solution but
    has been significantly modified to fit Linux's use in its integration.
    
    The same philosophy is borrowed, extended and further simplified:
    
    Linker tables enable an extremely light weight linker build time
    solution for feature ordering and selection, this can help to both
    simplify init sequences in a generic fashion and helps avoiding code
    bit-rotting when desirable.
    
    Bit rotting avoidance is enabled by *granting the option* to force
    compilation of code and only enable linking object code in when
    specific features have been enabled. It accomplishes this by using
    linker sections, the lightweight feature ordering of code is enabled
    by taking advantage of the old binutils ld SORT() on features
    specific sections.
    
    Contrary to iPXE's solution, which strives to force compilation
    of all features, Linux' solution strives to encourage usage of linker
    tables to force compilation of code *only on designated code*. Linker
    tables can be used without requiring one to force code compilation
    of features that use it though, to take advantage of the simplification
    of init sequences.
    
    Linux code that uses linker tables *and* wishes to always require
    compilation but selectively linking must use new kbuild target object
    that helps accomplishes this, table-y. Linux code that uses linker
    tables but does not want to require forcing compilation can use the
    good 'ol obj-y targets.
    
    Used commit 67a10ef000cb7 from iPXE upstream [0] as the starting point
    for development and evaluation of Linux's integration, further changes
    made and evaluation of different linker table options for Linux are
    available on the linker-table userspace tree [1].
    
    [0] git://git.ipxe.org/ipxe.git
    [1] https://git.kernel.org/cgit/linux/kernel/git/mcgrof/linker-tables.git/
    
    v3:
    
    o addressed initial modular support test cases
    o added generic asm macros so linker tables can be used in
      asm code / C asm calls
    o section ranges are now split up into their own set of files
    o use asm/sections.h instead of linux/sections.h for the linker
      script
    o add a sections.h file for each architecture that was missing one,
      this is needed now as we'll be relying on sections.h for custom
      section types in code rather than custom architecture specific
      linker script hacks.
    o full rewrite at this point, decided to pick copyleft-next license
      for this work
    
    v2:
    
    o modified completely to match feedback by community, made equivalent
      modifications to userspace solution. This is pretty much a complete
      rewrite of how we present and use linker tables. By using standard
      sections we no longer have to make custom linker script extensions
      for each new linker table solution, you just pick a linker table
      type by section type.
    o extend documention considerably, including use of kdoc
    o drop ICC hacks per popular request to ignore such issues for now
    o use sections.h - this lets us streamline a clean use case of
      well documented sections. To help further with this make use of
      SECTION_TBL() to allow use of these in code and SECTION_TBL_ALL()
      on linker scripts, as well as SECTION_TBL_ALL_STR() on relocs.c
      when needed.
    
    Cc: Michael Brown <mcb30@ipxe.org>
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

---
+++ arch/mn10300/include/asm/tables.h
@@ -0,0 +1,6 @@
+#ifndef _ASM_MN10300_TABLES_H
+#define _ASM_MN10300_TABLES_H
+
+#include <asm-generic/tables.h>
+
+#endif /* _ASM_MN10300_TABLES_H */
---
+++ arch/x86/include/asm/tables.h
@@ -0,0 +1,6 @@
+#ifndef _ASM_X86_TABLES_H
+#define _ASM_X86_TABLES_H
+
+#include <asm-generic/tables.h>
+
+#endif /* _ASM_X86_TABLES_H */
