a778074c4c0bfc942c4f3a8e52e58568673033d2
Luis R. Rodriguez <mcgrof@kernel.org>
2016-02-11 20:43:45 -0800

    firmware: port built-in section to linker table
    This ports built-in firmware to use linker tables,
    this replaces the custom section solution with a
    generic solution.
    
    This also demos the use of the .rodata (SECTION_RO)
    linker tables.
    
    Tested with 0 built-in firmware, 1 and 2 built-in
    firmwares successfully.
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -43,14 +43,13 @@ MODULE_LICENSE("GPL");
 
 #ifdef CONFIG_FW_LOADER
 
-extern struct builtin_fw __start_builtin_fw[];
-extern struct builtin_fw __end_builtin_fw[];
+DEFINE_LINKTABLE_RO(struct builtin_fw, builtin_fw);
 
 static bool fw_get_builtin_firmware(struct firmware *fw, const char *name)
 {
-	struct builtin_fw *b_fw;
+	const struct builtin_fw *b_fw;
 
-	for (b_fw = __start_builtin_fw; b_fw != __end_builtin_fw; b_fw++) {
+	LINKTABLE_FOR_EACH(b_fw, builtin_fw) {
 		if (strcmp(name, b_fw->name) == 0) {
 			fw->size = b_fw->size;
 			fw->data = b_fw->data;
@@ -63,9 +62,9 @@ static bool fw_get_builtin_firmware(struct firmware *fw, const char *name)
 
 static bool fw_is_builtin_firmware(const struct firmware *fw)
 {
-	struct builtin_fw *b_fw;
+	const struct builtin_fw *b_fw;
 
-	for (b_fw = __start_builtin_fw; b_fw != __end_builtin_fw; b_fw++)
+	LINKTABLE_FOR_EACH(b_fw, builtin_fw)
 		if (fw->data == b_fw->data)
 			return true;
 
