51b514eed965a0abf931bb4d844a0a376bea52b8
Luis R. Rodriguez <mcgrof@kernel.org>
2016-02-11 20:43:45 -0800

    firmware: port built-in section to linker table
    This ports built-in firmware to use linker tables,
    this replaces the custom section solution with a
    generic solution.
    
    This also demos the use of the .rodata linker table.
    
    Tested with 0 built-in firmware, 1 and 2 built-in
    firmwares successfully.
    
    v6: rename table macro as suggested by Andy Shevchenko
    
    v5:
    
    o since we dropped SECTION_ORDER_ANY, use 'any' order level explicitly
    
    v4:
    
    o work around c6x toolchain bug by using SECTION_TBL_RO
    
    o fix compilation on blackfin
    
    v3:
    o explicitly include tables.h as we no longer include
      tables.h from sections.h
    
    o use new section_tbl_asmtype() helper on firmware/Makefile
      to enable having to unfold things on our own.
    
    v2: introduced this file in this version of the series
    
    Cc: Barry Song <barry.song@analog.com>
    Cc: Mike Frysinger <vapier@gentoo.org>
    Cc: Steven Miao <realmz6@gmail.com>
    Cc: Michael Matz <matz@suse.de>
    Cc: Guenter Roeck <linux@roeck-us.net>
    Cc: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -31,6 +31,7 @@
 #include <linux/reboot.h>
 #include <linux/security.h>
 #include <linux/swait.h>
+#include <linux/tables.h>
 
 #include <generated/utsrelease.h>
 
@@ -44,15 +45,14 @@ MODULE_LICENSE("GPL");
 
 #ifdef CONFIG_FW_LOADER
 
-extern struct builtin_fw __start_builtin_fw[];
-extern struct builtin_fw __end_builtin_fw[];
+DEFINE_LINKTABLE_RO(struct builtin_fw, builtin_fw);
 
 static bool fw_get_builtin_firmware(struct firmware *fw, const char *name,
 				    void *buf, size_t size)
 {
-	struct builtin_fw *b_fw;
+	const struct builtin_fw *b_fw;
 
-	for (b_fw = __start_builtin_fw; b_fw != __end_builtin_fw; b_fw++) {
+	linktable_for_each(b_fw, builtin_fw) {
 		if (strcmp(name, b_fw->name) == 0) {
 			fw->size = b_fw->size;
 			fw->data = b_fw->data;
@@ -68,9 +68,9 @@ static bool fw_get_builtin_firmware(struct firmware *fw, const char *name,
 
 static bool fw_is_builtin_firmware(const struct firmware *fw)
 {
-	struct builtin_fw *b_fw;
+	const struct builtin_fw *b_fw;
 
-	for (b_fw = __start_builtin_fw; b_fw != __end_builtin_fw; b_fw++)
+	linktable_for_each(b_fw, builtin_fw)
 		if (fw->data == b_fw->data)
 			return true;
 
