5a0988dbef1635a4e88f0d62427ad11fa2dba44f
Luis R. Rodriguez <mcgrof@kernel.org>
2016-02-17 21:52:02 -0800

    jump_label: port __jump_table to linker tables
    Move the __jump_table from the a custom section solution
    to a generic solution, this avoiding extra vmlinux.lds.h
    customizations.
    
    This also demos the use of the .data (SECTION_DATA) linker
    table and of the shared asm call push_section_tbl().
    
    Built-in kernel functionality was tested with CONFIG_STATIC_KEYS_SELFTEST.
    Moduler  kernel functionality was tested with CONFIG_TEST_STATIC_KEYS.
    Both work as expected.
    
    Since __jump_table sections are also supported per
    module this also required expanding module-common.lds.S
    to capture and fold all .data.tlb.__jump_table.* onto
    the the section __jump_table -- in this case for modules
    need to keep a reference in place, given the alternative
    is to use DEFINE_LINKTABLE(struct jump_entry, __jump_table)
    per module -- and later through macro hacks instantiate
    the jump entries per module upon init. This is doable but
    we'd loose out on the sorting of the table using the
    linker, to sort we'd always still need to expand the
    module common linker script. An alternative mechanism
    is possible which would make these custom module sections
    extensions dynamic without requiring manual changes, this
    however is best done later through a separate evolution
    once linker tables are in place.
    
    A careful reviewer may note that some architectures use
    "\n\t" to separate asm code, while others just use a new line.
    Upon review last time it was deemed reasonable to for all
    architectures to just use "\n", this is defined as ASM_CMD_SEP,
    and if an architecture needs to override they can do so on their
    architecture sections.h prior to including asm-generic/sections.h
    
    v4:
    
    o Some architectures allow linker scripts to follow including header
      files, some others do not, so if you need a helper on a linker script
      you need to explicitly include it. So for instance although
      scripts/module-common.lds.S includes <asm/tables.h> and this file
      includes <asm/section-core.h>, you still need to explicitly
      include it on the linker script. This issue is present on ARM.
    
    o as per Josh Poimboeuf open code the section table name instead
      of including the kernel section headers, the simplicity and
      independence from the kernel is preferred.
    
    v3:
    
    o More elaborate tests performed
    o first modular support use case, module tested was
      CONFIG_TEST_STATIC_KEYS (lib/test_static_keys.ko), this
      required us to extend module-common.lds.S
    o use generic push_section_tbl_any() for all architectures
    o Makes use of ASM_CMD_SEP to enable architectures to override later
      if needed
    o guard tables.h inclusion and table definition with __KERNEL__
    
    v2: introduced in this series
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

--- arch/sparc/include/asm/jump_label.h
+++ arch/sparc/include/asm/jump_label.h
@@ -1,6 +1,8 @@
 #ifndef _ASM_SPARC_JUMP_LABEL_H
 #define _ASM_SPARC_JUMP_LABEL_H
 
+#include <asm/tables.h>
+
 #ifndef __ASSEMBLY__
 
 #include <linux/types.h>
@@ -12,7 +14,7 @@ static __always_inline bool arch_static_branch(struct static_key *key, bool bran
 	asm_volatile_goto("1:\n\t"
 		 "nop\n\t"
 		 "nop\n\t"
-		 ".pushsection __jump_table,  \"aw\"\n\t"
+		 push_section_tbl_any(SECTION_DATA, __jump_table, aw)
 		 ".align 4\n\t"
 		 ".word 1b, %l[l_yes], %c0\n\t"
 		 ".popsection \n\t"
@@ -28,7 +30,7 @@ static __always_inline bool arch_static_branch_jump(struct static_key *key, bool
 	asm_volatile_goto("1:\n\t"
 		 "b %l[l_yes]\n\t"
 		 "nop\n\t"
-		 ".pushsection __jump_table,  \"aw\"\n\t"
+		 push_section_tbl_any(SECTION_DATA, __jump_table, aw)
 		 ".align 4\n\t"
 		 ".word 1b, %l[l_yes], %c0\n\t"
 		 ".popsection \n\t"
