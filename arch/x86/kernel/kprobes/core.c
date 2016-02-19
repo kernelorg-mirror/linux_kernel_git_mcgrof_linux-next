c150ce953a9e7b3bbc486ecbd7efbafcd960f605
Luis R. Rodriguez <mcgrof@kernel.org>
2016-02-18 21:30:57 -0800

    kprobes: port .kprobes.text to section range
    kprobe makes use of two custom sections, each custom section
    is folded into one of the standard Linux sections types as follows,
    it currently relies on the linker script to fold the custom section
    onto the respective Linux section:
    
    type  Linux-section custom section name  begin                    end
    table .init.data    _kprobe_blacklist    __start_kprobe_blacklist __stop_kprobe_blacklist
    range .text         .kprobes.text        __kprobes_text_start     __kprobes_text_end
    
    This ports the .kprobes.text custom section to the standard
    Linux ranges API allowing us remove all the custom kprobe section
    declarations from the linker script.
    
    Tested with CONFIG_KPROBES_SANITY_TEST, it passes with:
    
    Kprobe smoke test: started
    Kprobe smoke test: passed successfully
    
    Then tested CONFIG_SAMPLE_KPROBES on do_fork, and the kprobe bites
    and kicks as expected.
    
    Also ran ./ftracetest with no issues:
    
    $ sudo ./ftracetest
    === Ftrace unit tests ===
    [1] Basic trace file check      [PASS]
    [2] Basic test for tracers      [PASS]
    [3] Basic trace clock test      [PASS]
    [4] Basic event tracing check   [PASS]
    [5] event tracing - enable/disable with event level files       [PASS]
    [6] event tracing - restricts events based on pid       [PASS]
    [7] event tracing - enable/disable with subsystem level files   [PASS]
    [8] event tracing - enable/disable with top level files [PASS]
    [9] ftrace - function graph filters with stack tracer   [PASS]
    [10] ftrace - function graph filters    [PASS]
    [11] ftrace - function profiler with function tracing   [PASS]
    [12] Test creation and deletion of trace instances while setting an event[PASS]
    [13] Test creation and deletion of trace instances      [PASS]
    [14] Kprobe dynamic event - adding and removing [PASS]
    [15] Kprobe dynamic event - busy event check    [PASS]
    [16] Kprobe dynamic event with arguments        [PASS]
    [17] Kprobe dynamic event with function tracer  [PASS]
    [18] Kretprobe dynamic event with arguments     [PASS]
    [19] event trigger - test event enable/disable trigger  [PASS]
    [20] event trigger - test trigger filter        [PASS]
    [21] event trigger - test histogram modifiers   [PASS]
    [22] event trigger - test histogram trigger     [PASS]
    [23] event trigger - test multiple histogram triggers   [PASS]
    [24] event trigger - test snapshot-trigger      [PASS]
    [25] event trigger - test stacktrace-trigger    [PASS]
    [26] event trigger - test traceon/off trigger   [PASS]
    
     # of passed:  26
     # of failed:  0
     # of unresolved:  0
     # of untested:  0
     # of unsupported:  0
     # of xfailed:  0
     # of undefined(test bug):  0
    
    v4:
    
    o open-code section use on scripts/ code -- folks to prefer the
      simplicity over dealing with having more tool code access kernel
      headers.
    
    o NOPE: include #include <asm-generic/ranges.h> on compiler.h -- solves
      a few 0-day compilation issues
    
    v3:
    
    o after v2 arch/arm/kernel/vmlinux-xip.lds.S got kprobe support,
      this just removes the custom linker script reference to kprobes as
      that is no longer needed with linker tables.
    
    o split kprobe linker table and kprobe section ranges use into
      two separate patches. This should make it easier to review and
      also demos both distinct use types, one a linker table another
      a simple section range.
    
    v2: introduced this patch in this series
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

--- arch/x86/kernel/kprobes/core.c
+++ arch/x86/kernel/kprobes/core.c
@@ -579,9 +579,9 @@ static int reenter_kprobe(struct kprobe *p, struct pt_regs *regs,
 	case KPROBE_REENTER:
 		/* A probe has been hit in the codepath leading up to, or just
 		 * after, single-stepping of a probed instruction. This entire
-		 * codepath should strictly reside in .kprobes.text section.
-		 * Raise a BUG or we'll continue in an endless reentering loop
-		 * and eventually a stack overflow.
+		 * codepath should strictly reside in the kprobes section
+		 * range. Raise a BUG or we'll continue in an endless
+		 * reentering loop and eventually a stack overflow.
 		 */
 		printk(KERN_WARNING "Unrecoverable kprobe detected at %p.\n",
 		       p->addr);
@@ -1128,10 +1128,9 @@ NOKPROBE_SYMBOL(longjmp_break_handler);
 
 bool arch_within_kprobe_blacklist(unsigned long addr)
 {
-	return  (addr >= (unsigned long)__kprobes_text_start &&
-		 addr < (unsigned long)__kprobes_text_end) ||
+	return  (SECTION_ADDR_IN_RANGE(kprobes, addr) ||
 		(addr >= (unsigned long)__entry_text_start &&
-		 addr < (unsigned long)__entry_text_end);
+		 addr < (unsigned long)__entry_text_end));
 }
 
 int __init arch_init_kprobes(void)
