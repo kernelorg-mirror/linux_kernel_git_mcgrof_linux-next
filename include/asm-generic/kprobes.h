15751260971955c7c2c54de6e08ab790c2cfc297
Luis R. Rodriguez <mcgrof@kernel.org>
2016-02-18 21:30:57 -0800

    kprobes: port .kprobes.text to section range
    kprobe makes use of two custom sections, each custom section
    is folded into one of the standard Linux sections types as follows,
    it currently relies on the linker script to fold the custom section
    onto the respective Linux section:
    
    type  Linux-section custom section name  begin                    end
    table .init.data    _kprobe_blacklist    __start_kprobe_blacklist __stop_kprobe_blacklist
    range .text         .kprobes.text        __kprobes_text_start     __kprobes_text_end
    
    This ports the .kprobes.text custom section to the standard
    Linux ranges API allowing us remove all the custom kprobe section
    declarations from the linker script.
    
    Tested with CONFIG_KPROBES_SANITY_TEST, it passes with:
    
    Kprobe smoke test: started
    Kprobe smoke test: passed successfully
    
    Then tested CONFIG_SAMPLE_KPROBES on do_fork, and the kprobe bites
    and kicks as expected.
    
    Also ran ./ftracetest with no issues:
    
    $ sudo ./ftracetest
    === Ftrace unit tests ===
    [1] Basic trace file check      [PASS]
    [2] Basic test for tracers      [PASS]
    [3] Basic trace clock test      [PASS]
    [4] Basic event tracing check   [PASS]
    [5] event tracing - enable/disable with event level files       [PASS]
    [6] event tracing - restricts events based on pid       [PASS]
    [7] event tracing - enable/disable with subsystem level files   [PASS]
    [8] event tracing - enable/disable with top level files [PASS]
    [9] ftrace - function graph filters with stack tracer   [PASS]
    [10] ftrace - function graph filters    [PASS]
    [11] ftrace - function profiler with function tracing   [PASS]
    [12] Test creation and deletion of trace instances while setting an event[PASS]
    [13] Test creation and deletion of trace instances      [PASS]
    [14] Kprobe dynamic event - adding and removing [PASS]
    [15] Kprobe dynamic event - busy event check    [PASS]
    [16] Kprobe dynamic event with arguments        [PASS]
    [17] Kprobe dynamic event with function tracer  [PASS]
    [18] Kretprobe dynamic event with arguments     [PASS]
    [19] event trigger - test event enable/disable trigger  [PASS]
    [20] event trigger - test trigger filter        [PASS]
    [21] event trigger - test histogram modifiers   [PASS]
    [22] event trigger - test histogram trigger     [PASS]
    [23] event trigger - test multiple histogram triggers   [PASS]
    [24] event trigger - test snapshot-trigger      [PASS]
    [25] event trigger - test stacktrace-trigger    [PASS]
    [26] event trigger - test traceon/off trigger   [PASS]
    
     # of passed:  26
     # of failed:  0
     # of unresolved:  0
     # of untested:  0
     # of unsupported:  0
     # of xfailed:  0
     # of undefined(test bug):  0
    
    v4:
    
    o open-code section use on scripts/ code -- folks to prefer the
      simplicity over dealing with having more tool code access kernel
      headers.
    
    o NOPE: include #include <asm-generic/ranges.h> on compiler.h -- solves
      a few 0-day compilation issues
    
    v3:
    
    o after v2 arch/arm/kernel/vmlinux-xip.lds.S got kprobe support,
      this just removes the custom linker script reference to kprobes as
      that is no longer needed with linker tables.
    
    o split kprobe linker table and kprobe section ranges use into
      two separate patches. This should make it easier to review and
      also demos both distinct use types, one a linker table another
      a simple section range.
    
    v2: introduced this patch in this series
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

--- include/asm-generic/kprobes.h
+++ include/asm-generic/kprobes.h
@@ -2,6 +2,7 @@
 #define _ASM_GENERIC_KPROBES_H
 
 #ifdef CONFIG_KPROBES
+#include <asm/ranges.h>
 /*
  * Blacklist ganerating macro. Specify functions which is not probed
  * by using this macro.
@@ -12,7 +13,7 @@ static unsigned long __used					\
 	_kbl_addr_##fname = (unsigned long)fname;
 # define NOKPROBE_SYMBOL(fname)	__NOKPROBE_SYMBOL(fname)
 /* Use this to forbid a kprobes attach on very low level functions */
-# define __kprobes	__attribute__((__section__(".kprobes.text")))
+# define __kprobes	__LINUX_RANGE(SECTION_TEXT, kprobes)
 # define nokprobe_inline	__always_inline
 #else
 # define NOKPROBE_SYMBOL(fname)
