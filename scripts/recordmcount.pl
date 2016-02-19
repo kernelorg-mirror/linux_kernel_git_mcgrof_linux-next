72831d350b651a898e0629b12fbab5a47282ac8a
Luis R. Rodriguez <mcgrof@kernel.org>
2016-02-18 21:30:57 -0800

    kprobes: port .kprobes.text to section range
    kprobe makes use of two custom sections, each custom section
    is folded into one of the standard Linux sections types as follows,
    it currently relies on the linker script to fold the custom section
    onto the respective Linux section:
    
    type  Linux-section custom section name  begin                    end
    table .init.data    _kprobe_blacklist    __start_kprobe_blacklist __stop_kprobe_blacklist
    range .text         .kprobes.text        __kprobes_text_start     __kprobes_text_end
    
    This ports the .kprobes.text custom section to the standard
    Linux ranges API allowing us remove all the custom kprobe section
    declarations from the linker script.
    
    Tested with CONFIG_KPROBES_SANITY_TEST, it passes with:
    
    Kprobe smoke test: started
    Kprobe smoke test: passed successfully
    
    Then tested CONFIG_SAMPLE_KPROBES on do_fork, and the kprobe bites
    and kicks as expected.
    
    Also ran ./ftracetest with no issues:
    
    sudo ./ftracetest
    === Ftrace unit tests ===
    [1] Basic trace file check	[PASS]
    [2] Basic test for tracers	[PASS]
    [3] Basic trace clock test	[PASS]
    [4] Basic event tracing check	[PASS]
    [5] event tracing - enable/disable with event level files	[PASS]
    [6] event tracing - restricts events based on pid	[PASS]
    [7] event tracing - enable/disable with subsystem level files	[PASS]
    [8] event tracing - enable/disable with top level files	[PASS]
    [9] ftrace - function graph filters with stack tracer	[PASS]
    [10] ftrace - function graph filters	[PASS]
    [11] ftrace - function glob filters	[PASS]
    [12] ftrace - function profiler with function tracing	[PASS]
    [13] Test creation and deletion of trace instances while setting an event	[PASS]
    [14] Test creation and deletion of trace instances	[PASS]
    [15] Kprobe dynamic event - adding and removing	[PASS]
    [16] Kprobe dynamic event - busy event check	[PASS]
    [17] Kprobe dynamic event with arguments	[PASS]
    [18] Kprobes event arguments with types	[PASS]
    [19] Kprobe dynamic event with function tracer	[PASS]
    [20] Kretprobe dynamic event with arguments	[PASS]
    [21] event trigger - test event enable/disable trigger	[PASS]
    [22] event trigger - test trigger filter	[PASS]
    [23] event trigger - test histogram modifiers	[PASS]
    [24] event trigger - test histogram trigger	[PASS]
    [25] event trigger - test multiple histogram triggers	[PASS]
    [26] event trigger - test snapshot-trigger	[PASS]
    [27] event trigger - test stacktrace-trigger	[PASS]
    [28] event trigger - test traceon/off trigger	[PASS]
    
    v5:
    
    o Use ..rng.* instead of .rng.* as suggested by Nicholas Piggin.
      This is the typical way to avoid clash with compiler generated
      section.
    
    o Replace section macros with section names
    
    v4:
    
    o arm64 build fixes with allmodconfig
    
    o build fix suggested for avr32 with allnoconfig, otherwise we end up with:
    
    arch/avr32/kernel/built-in.o: In function `save_full_context_ex':
    (.ex.text+0x1c4): relocation truncated to fit: R_AVR32_16N_PCREL against
    symbol `debug_trampoline' defined in .text.rng.kprobes.any section in
    arch/avr32/kernel/built-in.o
    arch/avr32/kernel/built-in.o: In function `debug_exit_work':
    (.text.rng.kprobes.any+0xa8): relocation truncated to fit:
    R_AVR32_16N_PCREL against `.ex.text'+3aa
    make: *** [Makefile:953: vmlinux] Error 1
    
    o open-code section use on scripts/ code -- folks to prefer the
      simplicity over dealing with having more tool code access kernel
      headers.
    
    o NOPE: include #include <asm-generic/ranges.h> on compiler.h -- solves
      a few 0-day compilation issues
    
    v3:
    
    o after v2 arch/arm/kernel/vmlinux-xip.lds.S got kprobe support,
      this just removes the custom linker script reference to kprobes as
      that is no longer needed with linker tables.
    
    o split kprobe linker table and kprobe section ranges use into
      two separate patches. This should make it easier to review and
      also demos both distinct use types, one a linker table another
      a simple section range.
    
    v2: introduced this patch in this series
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

--- scripts/recordmcount.pl
+++ scripts/recordmcount.pl
@@ -135,7 +135,7 @@ my %text_sections = (
      ".spinlock.text" => 1,
      ".irqentry.text" => 1,
      ".softirqentry.text" => 1,
-     ".kprobes.text" => 1,
+     ".text..rng.kprobes.any" => 1,
      ".cpuidle.text" => 1,
      ".text.unlikely" => 1,
 );
