b86bcfc48634e5ca8bddafe5fdd0ea471386e9f0
Luis R. Rodriguez <mcgrof@kernel.org>
2016-04-28 17:02:01 -0700

    p54: convert to sysdata API
    The Coccinelle sysdata patches were used to help with
    this transition. The changes have been carefully manually
    vetted for. With the conversion we modify the cases that do
    not need the firmware to be kept so that the sysdata API
    can release it for us. Using the new sysdata API also means
    we can get rid of our own completions.
    
    v2: was not present
    v3: initial release
    v4: small cosmetic fixes
    v5: bike shed changes
    
    Generated-by: Coccinelle SmPL
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

--- drivers/net/wireless/intersil/p54/fwio.c
+++ drivers/net/wireless/intersil/p54/fwio.c
@@ -17,7 +17,7 @@
  */
 
 #include <linux/slab.h>
-#include <linux/firmware.h>
+#include <linux/drvdata.h>
 #include <linux/etherdevice.h>
 #include <linux/export.h>
 
@@ -27,7 +27,8 @@
 #include "eeprom.h"
 #include "lmac.h"
 
-int p54_parse_firmware(struct ieee80211_hw *dev, const struct firmware *fw)
+int p54_parse_firmware(struct ieee80211_hw *dev,
+		       const struct drvdata *fw)
 {
 	struct p54_common *priv = dev->priv;
 	struct exp_if *exp_if;
