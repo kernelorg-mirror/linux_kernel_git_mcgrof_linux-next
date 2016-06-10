876090b413b1cae9f0be48d76df8341a02bc9b53
Luis R. Rodriguez <mcgrof@kernel.org>
2016-06-10 11:08:59 -0700

    scripts: add glimpse.sh for indexing the kernel
    Glimpse is a tool you can use to index the kernel. The tool
    was open sourced on 2014-09-26 [0] under the ISC license however
    the original release still [1] does not compile. A fix for this
    and a release that does compile is provided in a temporary
    tree [2].
    
    v2 changesl:
    o simplify DIR in one line and add verbose release information
      to commit log about the open sourcing of glimpse and its
      current status.
    o add index files to .gitignore
    
    [0] http://webglimpse.net/
    [1] https://github.com/gvelez17/glimpse/
    [2] https://github.com/mcgrof/glimpse.git
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

---
+++ scripts/glimpse.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR="$(dirname $(readlink -f $0))/.."
+
+GLIMPSEINDEX="`which ${GLIMPSEINDEX:=glimpseindex}`"
+if [ ! -x "$GLIMPSEINDEX" ]; then
+	echo 'glimpseindex can be obtained at https://github.com/mcgrof/glimpse.git'
+	exit 1
+fi
+
+find $DIR/* -name "*.[ch]" | $GLIMPSEINDEX -o -H . -F
