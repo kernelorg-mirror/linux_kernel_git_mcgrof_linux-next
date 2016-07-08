962c88e92da136162e0daf2cd7df529830639fd9
Luis R. Rodriguez <mcgrof@kernel.org>
2016-07-07 14:37:35 -1000

    ranges.h: add helpers to build and identify Linux section ranges
    Section ranges are on one of the types of custom sections
    types used in Linux. This provides a series of helpers for
    defining them and using them. Most importantly this also
    enables us to avoid modifying the linker script when we
    add new section range.
    
    It turns out a lot of custom sections are actually section ranges,
    and these are typically spelled out in their architecture specific
    asm/sections.h file -- we anable architectures to override what asm
    is used for section ranges but start by defult trusting the asm-generic
    version all around.
    
    v3: new in this series, uses copyleft-next
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

---
+++ arch/mips/include/asm/ranges.h
@@ -0,0 +1,6 @@
+#ifndef _ASM_MIPS_RANGES_H
+#define _ASM_MIPS_RANGES_H
+
+#include <asm-generic/ranges.h>
+
+#endif /* _ASM_MIPS_RANGES_H */
---
+++ arch/s390/include/asm/ranges.h
@@ -0,0 +1,6 @@
+#ifndef _ASM_S390_RANGES_H
+#define _ASM_S390_RANGES_H
+
+#include <asm-generic/ranges.h>
+
+#endif /* _ASM_S390_RANGES_H */
