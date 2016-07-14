98db63838238c57cc1ee447f86a6fb8431500299
Luis R. Rodriguez <mcgrof@kernel.org>
2016-07-14 14:39:30 -0700

    sections.h: guard against asm and linker script
    We'll later add some generic helpers for use in the linker scripts
    and some generic asm code for all architectures. To do that we'll
    first need to guard against linker script and asm code.
    
    On x86 uaccess.h has a struct which is used as part of a section,
    move that to sections.h as we otherwise have no need for uaccess.h
    on sections.h
    
    v3: new to this series, needed due to the collateral of the split of
        tables.h into 3 files: tables.h, ranges.h and sections.h, the
        need to guard sections.h is due to the fact that sections.h is
        already included and expanded considerably in certain architectures.
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

--- arch/x86/include/asm/sections.h
+++ arch/x86/include/asm/sections.h
@@ -2,13 +2,34 @@
 #define _ASM_X86_SECTIONS_H
 
 #include <asm-generic/sections.h>
-#include <asm/uaccess.h>
+
+#if defined(__KERNEL__) && !defined(__ASSEMBLER__) && !defined(__ASSEMBLY__)
 
 extern char __brk_base[], __brk_limit[];
+
+/*
+ * The exception table consists of triples of addresses relative to the
+ * exception table entry itself. The first address is of an instruction
+ * that is allowed to fault, the second is the target at which the program
+ * should continue. The third is a handler function to deal with the fault
+ * caused by the instruction in the first field.
+ *
+ * All the routines below use bits of fixup code that are out of line
+ * with the main instruction path.  This means when everything is well,
+ * we don't even have to jump over them.  Further, they do not intrude
+ * on our cache or tlb entries.
+ */
+
+struct exception_table_entry {
+	int insn, fixup, handler;
+};
+
 extern struct exception_table_entry __stop___ex_table[];
 
 #if defined(CONFIG_X86_64)
 extern char __end_rodata_hpage_align[];
 #endif
+
+#endif /* defined(__KERNEL__) && !defined(__ASSEMBLER__) && !defined(__ASSEMBLY__) */
 
 #endif	/* _ASM_X86_SECTIONS_H */
