db31cd8011ea1968b097f168e57715905342fb5e
Luis R. Rodriguez <mcgrof@kernel.org>
2016-07-14 14:39:30 -0700

    sections.h: guard against asm and linker script
    We'll later add some generic helpers for use in the linker scripts
    and some generic asm code for all architectures. To do that we'll
    first need to guard against linker script and asm code.
    
    On x86 uaccess.h has a struct which is used as part of a section,
    move that to sections.h as we otherwise have no need for uaccess.h
    on sections.h
    
    v3: new to this series, needed due to the collateral of the split of
        tables.h into 3 files: tables.h, ranges.h and sections.h, the
        need to guard sections.h is due to the fact that sections.h is
        already included and expanded considerably in certain architectures.
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

--- arch/powerpc/include/asm/sections.h
+++ arch/powerpc/include/asm/sections.h
@@ -1,11 +1,12 @@
 #ifndef _ASM_POWERPC_SECTIONS_H
 #define _ASM_POWERPC_SECTIONS_H
-#ifdef __KERNEL__
+
+#include <asm-generic/sections.h>
+
+#if defined(__KERNEL__) && !defined(__ASSEMBLER__) && !defined(__ASSEMBLY__)
 
 #include <linux/elf.h>
 #include <linux/uaccess.h>
-#include <asm-generic/sections.h>
-
 #ifdef __powerpc64__
 
 extern char __start_interrupts[];
@@ -75,7 +76,7 @@ static inline void *dereference_function_descriptor(void *ptr)
 }
 #endif /* PPC64_ELF_ABI_v1 */
 
-#endif
+#endif /* __powerpc64__ */
+#endif /* defined(__KERNEL__) && !defined(__ASSEMBLER__) && !defined(__ASSEMBLY__) */
 
-#endif /* __KERNEL__ */
 #endif	/* _ASM_POWERPC_SECTIONS_H */
