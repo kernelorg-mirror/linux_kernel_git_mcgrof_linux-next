dbd72fd1bb050f7a6b210987fb993b3f8b46e213
Luis R. Rodriguez <mcgrof@kernel.org>
2016-08-18 16:22:26 -0700

    tools: add init.h for tools
    Start off with just __ref -- we enalbe you to override, if you do
    that then you can define your own. The way you'd use this, if you
    do override, is define your own __ref and then use include_next.
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

---
+++ tools/include/linux/init.h
@@ -0,0 +1,9 @@
+#ifndef _TOOLS_LINUX_INIT_H
+#define _TOOLS_LINUX_INIT_H
+
+/* this means you can add your own to fit you own userspace needs */
+#ifndef __ref
+#define __ref
+#endif
+
+#endif /* _TOOLS_LINUX_INIT_H */
