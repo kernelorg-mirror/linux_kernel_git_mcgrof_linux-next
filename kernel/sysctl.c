01e6cd5f8aefad506f42d8e83cf8efb271b16f9f
Luis Chamberlain <mcgrof@kernel.org>
2020-05-27 13:40:51 -0600

    sysctl: share unsigned long const values
    Provide a way to share unsigned long values.
    This will allow others to not have to re-invent
    these values.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -97,9 +97,6 @@
 
 /* Constants used for minimum and  maximum */
 
-static const unsigned long zero_ul;
-static const unsigned long one_ul = 1;
-static const unsigned long long_max = LONG_MAX;
 #ifdef CONFIG_PRINTK
 static const int ten_thousand = 10000;
 #endif
@@ -2437,7 +2434,7 @@ static struct ctl_table vm_table[] = {
 		.maxlen		= sizeof(dirty_background_bytes),
 		.mode		= 0644,
 		.proc_handler	= dirty_background_bytes_handler,
-		.extra1		= (void *)&one_ul,
+		.extra1		= SYSCTL_LONG_ONE,
 	},
 	{
 		.procname	= "dirty_ratio",
@@ -2846,8 +2843,8 @@ static struct ctl_table fs_table[] = {
 		.maxlen		= sizeof(files_stat.max_files),
 		.mode		= 0644,
 		.proc_handler	= proc_doulongvec_minmax,
-		.extra1		= (void *)&zero_ul,
-		.extra2		= (void *)&long_max,
+		.extra1		= SYSCTL_LONG_ZERO,
+		.extra2		= SYSCTL_LONG_MAX,
 	},
 	{
 		.procname	= "nr_open",
