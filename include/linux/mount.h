102093c1d4c075c165a83d2b69ecd64507a6cbe1
Luis Chamberlain <mcgrof@kernel.org>
2020-05-27 19:55:44 -0600

    fs: move namespace sysctls and declare fs base directory
    This moves the namespace sysctls to its own file as part of the
    kernel/sysctl.c spring cleaning
    
    Since we have now removed all sysctls for "fs", we now have to
    declare it on the filesystem code, we do that using the new helper,
    which reduces boiler plate code.
    
    We rename init_fs_shared_sysctls() to init_fs_sysctls() to reflect
    that now fs/sysctls.c is taking on the burden of being te first
    to register the base directory as well.
    
    Lastly, since init code will load in the order in which we link it
    we have to move the sysctl code to be linked in early, so that its
    early init routine runs prior to other fs code. This way, other
    filesystem code can register their own sysctls using the helpers:
    
      * register_sysctl_init()
      * register_sysctl_subdir()
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- include/linux/mount.h
+++ include/linux/mount.h
@@ -106,9 +106,6 @@ extern void mnt_set_expiry(struct vfsmount *mnt, struct list_head *expiry_list);
 extern void mark_mounts_for_expiry(struct list_head *mounts);
 
 extern dev_t name_to_dev_t(const char *name);
-
-extern unsigned int sysctl_mount_max;
-
 extern bool path_is_mountpoint(const struct path *path);
 
 #endif /* _LINUX_MOUNT_H */
