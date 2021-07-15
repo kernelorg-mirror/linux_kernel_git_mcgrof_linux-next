ba96f4b977ceb2cffdb8df97fb56a1d2e2538c3c
Luis Chamberlain <mcgrof@kernel.org>
2021-07-15 02:27:19 +0000

    md: replace GENHD_FL_UP with GENHD_FL_DISK_ADDED on is_mddev_broken()
    The GENHD_FL_DISK_ADDED flag is what we really want, as the
    flag GENHD_FL_UP could be set on a semi-initialized device.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- drivers/md/md.h
+++ drivers/md/md.h
@@ -766,7 +766,7 @@ static inline bool is_mddev_broken(struct md_rdev *rdev, const char *md_type)
 {
 	int flags = rdev->bdev->bd_disk->flags;
 
-	if (!(flags & GENHD_FL_UP)) {
+	if (!(flags & GENHD_FL_DISK_ADDED)) {
 		if (!test_and_set_bit(MD_BROKEN, &rdev->mddev->flags))
 			pr_warn("md: %s: %s array has a missing/failed member\n",
 				mdname(rdev->mddev), md_type);
