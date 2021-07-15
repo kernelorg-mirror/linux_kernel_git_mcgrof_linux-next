be2b211a6f3059ab1033f630679d6cff339044f2
Luis Chamberlain <mcgrof@kernel.org>
2021-07-15 00:55:32 +0000

    lightnvm: add error handling support for add_disk()
    We never checked for errors on add_disk() as this function
    returned void. Now that this is fixed, use the shiny new
    error handling.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- drivers/lightnvm/core.c
+++ drivers/lightnvm/core.c
@@ -397,7 +397,9 @@ static int nvm_create_tgt(struct nvm_dev *dev, struct nvm_ioctl_create *create)
 	blk_queue_max_hw_sectors(tdisk->queue, mdts);
 
 	set_capacity(tdisk, tt->capacity(targetdata));
-	add_disk(tdisk);
+	ret = add_disk(tdisk);
+	if (ret)
+		goto err_init;
 
 	if (tt->sysfs_init && tt->sysfs_init(tdisk)) {
 		ret = -ENOMEM;
