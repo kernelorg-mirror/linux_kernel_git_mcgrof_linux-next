da898d13309d7b2855a74ee4e49e0fb43a55a2f1
Luis Chamberlain <mcgrof@kernel.org>
2021-08-13 14:58:11 -0700

    sysfs: fix deadlock race with module removal
    When sysfs attributes use a lock also used on module removal we can
    race to deadlock. This happens when for instance a sysfs file on
    a driver is used, then at the same time we have module removal call
    trigger. The module removal call code holds a lock, and then the sysfs
    file entry waits for the same lock. While holding the lock the module
    removal tries to remove the sysfs entries, but these cannot be removed
    yet as one is waiting for a lock. This won't complete as the lock is
    already held. Likewise module removal cannot complete, and so we deadlock.
    
    This can now be easily reproducible with our sysfs selftest as follows:
    
    ./tools/testing/selftests/sysfs/sysfs.sh -t 0027
    
    To fix this we just *try* to get a refcount to the module when a shared
    lock is used, prior to mucking with a sysfs attribute. If this fails we
    just give up right away.
    
    We use a try method as a full lock means we'd then make our sysfs
    attributes busy us out from possible module removal, and so userspace
    could force denying module removal, a silly form of "DOS" against module
    removal. A try lock on the module removal ensures we give priority to
    module removal and interacting with sysfs attributes only comes second.
    Using a full lock could mean for instance that if you don't stop poking
    at sysfs files you cannot remove a module.
    
    Races between removal of sysfs files and the module are not possible
    given sysfs files are created by the same module, and when a sysfs file
    is being used kernfs prevents removal of the sysfs file. So if module
    removal is actually happening the removal would have to wait until
    the sysfs file is done being used.
    
    This deadlock was first reported with the zram driver, however the live
    patching folks have acknowledged they have observed this as well with
    live patching, when a live patch is removed. I was then able to
    reproduce easily by creating a dedicated selftests.
    
    A sketch of how this can happen follows:
    
    CPU A                              CPU B
                                       whatever_store()
    module_unload
      mutex_lock(foo)
                                       mutex_lock(foo)
       del_gendisk(zram->disk);
         device_del()
           device_remove_groups()
    
    In this situation whatever_store() is waiting for the mutex foo to
    become unlocked, but that won't happen until module removal is complete.
    But module removal won't complete until the sysfs file being poked
    completes which is waiting for a lock already held.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- fs/sysfs/file.c
+++ fs/sysfs/file.c
@@ -314,8 +314,8 @@ int sysfs_add_file_mode_ns(struct kernfs_node *parent,
 		key = attr->key ?: (struct lock_class_key *)&attr->skey;
 #endif
 
-	kn = __kernfs_create_file(parent, attr->name, mode & 0777, uid, gid,
-				  size, ops, (void *)attr, ns, key);
+	kn = __kernfs_create_file(parent, attr->name, mode & 0777, attr->owner,
+				  uid, gid, size, ops, (void *)attr, ns, key);
 	if (IS_ERR(kn)) {
 		if (PTR_ERR(kn) == -EEXIST)
 			sysfs_warn_dup(parent, attr->name);
