19ac402ca5e544129068529441f86c26875bb50f
Luis Chamberlain <mcgrof@kernel.org>
2021-09-03 22:38:20 +0000

    firmware_loader: formalize built-in firmware API
    Now that we have a kconfig entry to represent built-in firmware
    support, formalize its use witha a proper API. This can later be
    used by other callers where all they need is built-in firmware.
    
    We export the firmware_request_builtin() call for now only
    under the TEST_FIRMWARE symbol namespace as there are no
    direct modular users for it. If they pop up they are free
    to export it generally. Built-in code always gets access to
    the callers and we'll demonstrate a hidden user which has been
    lurking in the kernel for a while and the reason why using a
    proper API was better long term.
    
    Reviewed-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

---
+++ drivers/base/firmware_loader/builtin/main.c
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Builtin firmware support */
+
+#include <linux/firmware.h>
+#include "../firmware.h"
+
+extern struct builtin_fw __start_builtin_fw[];
+extern struct builtin_fw __end_builtin_fw[];
+
+static void fw_copy_to_prealloc_buf(struct firmware *fw,
+				    void *buf, size_t size)
+{
+	if (!buf || size < fw->size)
+		return;
+	memcpy(buf, fw->data, fw->size);
+}
+
+/**
+ * firmware_request_builtin() - load builtin firmware
+ * @firmware_p: stack pointer to local firmware struct
+ * @name: name of firmware file
+ *
+ * Some use cases in the kernel require looking for firmware which is built-in
+ * to the kernel, and also have a requirement so that no memory allocator is
+ * involved as these calls take place early in boot process. An example is the
+ * x86 CPU microcode loader. In these cases all the caller wants is to see if
+ * the firmware was built-in and if so use it right away.
+ *
+ * Callers of this API do not need to use release_firmware() as the pointer to
+ * the firmware is expected to be provided locally on the stack of the caller.
+ **/
+bool firmware_request_builtin(struct firmware *fw, const char *name,
+			      void *buf, size_t size)
+{
+	struct builtin_fw *b_fw;
+
+	for (b_fw = __start_builtin_fw; b_fw != __end_builtin_fw; b_fw++) {
+		if (strcmp(name, b_fw->name) == 0) {
+			fw->size = b_fw->size;
+			fw->data = b_fw->data;
+			fw_copy_to_prealloc_buf(fw, buf, size);
+
+			return true;
+		}
+	}
+
+	return false;
+}
+EXPORT_SYMBOL_NS_GPL(firmware_request_builtin, TEST_FIRMWARE);
+
+bool firmware_is_builtin(const struct firmware *fw)
+{
+	struct builtin_fw *b_fw;
+
+	for (b_fw = __start_builtin_fw; b_fw != __end_builtin_fw; b_fw++)
+		if (fw->data == b_fw->data)
+			return true;
+
+	return false;
+}
