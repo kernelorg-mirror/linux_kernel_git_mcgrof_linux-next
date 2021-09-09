0b2bc4f1e47eaca419882b44793f480e77d716e1
Luis Chamberlain <mcgrof@kernel.org>
2021-09-09 12:27:15 -0700

    test_firmare: extend builtin testing with x86 microcode
    The linux-firmware tree has some microcode files which
    don't update often. These files are also uniquely supported
    by the x86 microcode update code and are currently the only
    users of the builtin firmware API. Test for these as good
    examples candidates. This helps extend coverage of testing
    for built-in firmware support and helps also ensure that if
    microcode updates are supported that can get tested as well
    in parallel.
    
    This testing ensures that if these tests are OK but something
    else is failing with the microcode update code, then the issue
    is *not* the firmware API as we are guaranteeing fidelity with
    the expected microcode files.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- tools/testing/selftests/firmware/fw_builtin.sh
+++ tools/testing/selftests/firmware/fw_builtin.sh
@@ -25,6 +25,11 @@ if [[ "$HAS_FW_LOADER_BUILTIN" != "yes" || "$HAS_TEST_FIRMWARE_BUILTIN" != "yes"
 	exit $ksft_skip
 fi
 
+MICROCODE_BUILTIN_AMD_DIR="amd-ucode"
+if [[ -f $PROC_CONFIG && "$(kconfig_has CONFIG_MICROCODE_BUILTIN_AMD=y)" == "yes" ]]; then
+	MICROCODE_BUILTIN_AMD=$(zgrep ^CONFIG_MICROCODE_BUILTIN_AMD_DIR $PROC_CONFIG | awk -F"=" '{print $2}')
+fi
+
 echo "Testing builtin firmware API ... "
 
 config_trigger_builtin()
@@ -60,8 +65,78 @@ test_builtin_firmware_nofile()
 	echo "OK"
 }
 
+test_builtin_firmware_microcode()
+{
+	MICROCODE="$1"
+	MICROCODE_MD5SUM="$2"
+	CONFIG_GREP="$3"
+
+	echo -n "Testing firmware_request_builtin() with $MICROCODE ..."
+	config_reset
+	if [[ "$(kconfig_has CONFIG_MICROCODE_BUILTIN_AMD_10H_14H=y)" != "yes" ]]; then
+		echo "Skipping as its not built-in"
+	fi
+	config_set_name $MICROCODE
+	config_trigger_builtin
+	echo OK
+
+	# Verify the contents are what we expect.
+	echo -n "Verifying file md5sum ... "
+
+	BUILTIN_MD5SUM=$(md5sum /dev/test_firmware | awk '{print $1}')
+	if [[ "$MICROCODE_MD5SUM" != "$BUILTIN_MD5SUM" ]]; then
+		echo "failed, expected and got the following" >&2
+		echo "$MICROCODE_MD5SUM" >&2
+		echo "$BUILTIN_MD5SUM" >&2
+		exit 1
+	else
+		echo "$BUILTIN_MD5SUM OK!"
+	fi
+}
+
+test_builtin_firmware_amd_10h_14h()
+{
+	MY_MICROCODE="$MICROCODE_BUILTIN_AMD_DIR/microcode_amd.bin"
+	MY_MICROCODE_MD5SUM="55ae79b82cbfddcf7142058be3c9ec2d"
+	MY_CONFIG_SYMBOL="CONFIG_MICROCODE_BUILTIN_AMD_10H_14H"
+
+	test_builtin_firmware_microcode $MY_MICROCODE $MY_MICROCODE_MD5SUM $MY_CONFIG_SYMBOL
+}
+
+test_builtin_firmware_amd_15h()
+{
+	MY_MICROCODE="$MICROCODE_BUILTIN_AMD_DIR/microcode_amd_fam15h.bin"
+	MY_MICROCODE_MD5SUM="3bdedb4466186a79c469f62120f6d7bb"
+	MY_CONFIG_SYMBOL="CONFIG_MICROCODE_BUILTIN_AMD_15H"
+
+	test_builtin_firmware_microcode $MY_MICROCODE $MY_MICROCODE_MD5SUM $MY_CONFIG_SYMBOL
+}
+
+test_builtin_firmware_amd_16h()
+{
+	MY_MICROCODE="$MICROCODE_BUILTIN_AMD_DIR/microcode_amd_fam16h.bin"
+	MY_MICROCODE_MD5SUM="6a47a6393c52ddfc0b5b044efc076a77"
+	MY_CONFIG_SYMBOL="CONFIG_MICROCODE_BUILTIN_AMD_16H"
+
+	test_builtin_firmware_microcode $MY_MICROCODE $MY_MICROCODE_MD5SUM $MY_CONFIG_SYMBOL
+}
+
+test_builtin_firmware_amd_17h()
+{
+	MY_MICROCODE="$MICROCODE_BUILTIN_AMD_DIR/microcode_amd_fam17h.bin"
+	MY_MICROCODE_MD5SUM="60f18b6d7fa3d1231b27cc339c173c8c"
+	MY_CONFIG_SYMBOL="CONFIG_MICROCODE_BUILTIN_AMD_17H"
+
+	test_builtin_firmware_microcode $MY_MICROCODE $MY_MICROCODE_MD5SUM $MY_CONFIG_SYMBOL
+}
+
 test_builtin_firmware
 test_builtin_firmware_nofile
+
+test_builtin_firmware_amd_10h_14h
+test_builtin_firmware_amd_15h
+test_builtin_firmware_amd_16h
+test_builtin_firmware_amd_17h
 
 # Ensure test_fw_config->is_builtin is set back to false
 # otherwise we won't be able to diff against the right target
