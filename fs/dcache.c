871395e67190d1124844743cc01d67225cb363f5
Luis Chamberlain <mcgrof@kernel.org>
2021-11-17 15:43:23 -0800

    fs: move dcache sysctls to its own file
    Everyone keeps stuffing their sysctls to kernel/sysctl.c as if it
    was a kitchen sink, and this creates a maintenance burden.
    
    Remove the dcache sysctl clutter out of kernel/sysctl.
    This is a one-off entry, perhaps later we can simplify this
    representation, but for now we follow the helpers we have.
    We won't know how we can simplify this further untl we're
    fully done with the cleanup.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- fs/dcache.c
+++ fs/dcache.c
@@ -167,14 +167,32 @@ static long get_nr_dentry_negative(void)
 	return sum < 0 ? 0 : sum;
 }
 
-int proc_nr_dentry(struct ctl_table *table, int write, void *buffer,
-		   size_t *lenp, loff_t *ppos)
+static int proc_nr_dentry(struct ctl_table *table, int write, void *buffer,
+			  size_t *lenp, loff_t *ppos)
 {
 	dentry_stat.nr_dentry = get_nr_dentry();
 	dentry_stat.nr_unused = get_nr_dentry_unused();
 	dentry_stat.nr_negative = get_nr_dentry_negative();
 	return proc_doulongvec_minmax(table, write, buffer, lenp, ppos);
 }
+
+static struct ctl_table fs_dcache_sysctls[] = {
+	{
+		.procname	= "dentry-state",
+		.data		= &dentry_stat,
+		.maxlen		= 6*sizeof(long),
+		.mode		= 0444,
+		.proc_handler	= proc_nr_dentry,
+	},
+	{ }
+};
+
+static int __init init_fs_dcache_sysctls(void)
+{
+	register_sysctl_init("fs", fs_dcache_sysctls, "dcache");
+	return 0;
+}
+early_initcall(init_fs_dcache_sysctls);
 #endif
 
 /*
