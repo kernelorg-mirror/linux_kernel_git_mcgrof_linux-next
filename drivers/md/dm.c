9919275b026b252fa6193b9e1e2021776f134d75
Pankaj Raghav <p.raghav@samsung.com>
2022-06-15 12:19:19 +0200

    dm: call dm_zone_endio after the target endio callback for zoned devices
    dm_zone_endio() updates the bi_sector of orig bio for zoned devices that
    uses either native append or append emulation and it is called before the
    endio of the target. But target endio can still update the clone bio
    after dm_zone_endio is called, thereby, the orig bio does not contain
    the updated information anymore. Call dm_zone_endio for zoned devices
    after calling the target's endio function
    
    Signed-off-by: Pankaj Raghav <p.raghav@samsung.com>
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>
    [rebased onto next-20220707 and ajust commit log]

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -1123,10 +1123,6 @@ static void clone_endio(struct bio *bio)
 			disable_write_zeroes(md);
 	}
 
-	if (static_branch_unlikely(&zoned_enabled) &&
-	    unlikely(bdev_is_zoned(bio->bi_bdev)))
-		dm_zone_endio(io, bio);
-
 	if (endio) {
 		int r = endio(ti, bio, &error);
 		switch (r) {
@@ -1154,6 +1150,10 @@ static void clone_endio(struct bio *bio)
 			BUG();
 		}
 	}
+
+	if (static_branch_unlikely(&zoned_enabled) &&
+	    unlikely(bdev_is_zoned(bio->bi_bdev)))
+		dm_zone_endio(io, bio);
 
 	if (static_branch_unlikely(&swap_bios_enabled) &&
 	    unlikely(swap_bios_limit(ti, bio)))
