795c4afac17d26300d95d0fc0b772c3d9bab35bb
Luis Chamberlain <mcgrof@kernel.org>
2023-03-27 11:56:11 -0700

    nvme: allow a debug module parameter to enable LBAs > PAGE_SIZE
    As discused in preperation for LSFMM [0] enabling LBAs > PAGE_SIZE
    triggers a crash with a null pointer dereference. If we want to get to
    the poin to enable an NVMe device with a larger logical block size
    greater than PAGE_SIZE we need to work out the kinks to support this.
    
    Enable a module parameter for nvme_core to enable experimentation with
    these types of devices.
    
    [0] https://lkml.kernel.org/r/ZAJvu2hZrHu816gj@kbusch-mbp.dhcp.thefacebook.com
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -88,6 +88,10 @@ module_param(apst_secondary_latency_tol_us, ulong, 0644);
 MODULE_PARM_DESC(apst_secondary_latency_tol_us,
 	"secondary APST latency tolerance in us");
 
+static bool debug_large_lbas;
+module_param(debug_large_lbas, bool, 0644);
+MODULE_PARM_DESC(debug_large_lbas, "allow LBAs > PAGE_SIZE");
+
 /*
  * nvme_wq - hosts nvme related works that are not reset or delete
  * nvme_reset_wq - hosts nvme reset works
@@ -1841,10 +1845,17 @@ static void nvme_update_disk_info(struct gendisk *disk,
 	/*
 	 * The block layer can't support LBA sizes larger than the page size
 	 * yet, so catch this early and don't allow block I/O.
+	 *
+	 * The issues for LBA > PAGE_SIZE need to be fixed.
 	 */
 	if (ns->lba_shift > PAGE_SHIFT) {
-		capacity = 0;
-		bs = (1 << 9);
+		if (debug_large_lbas)
+			dev_warn(ns->ctrl->device,
+				 "forcibly allowing LBAS > PAGE_SIZE due to nvme_core.debug_large_lbas -- use at your own risk\n");
+		else {
+			capacity = 0;
+			bs = (1 << 9);
+		}
 	}
 
 	blk_integrity_unregister(disk);
