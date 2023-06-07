e7ea5b5fcb16eb4aa285cc93b39748a7a28f6ae4
Luis Chamberlain <mcgrof@kernel.org>
2023-06-07 11:36:14 -0700

    nvme: add nvme_core.debug_large_atomics to force high awun as phys_bs
    A drive with atomic write support should have awun / nawun defined,
    for these drives it should be possible to play with and experiment
    safely with LBS support up to awun / nawun settings if you are
    completely ignoring power failure situations. Add support to
    experiment with this. The rationale to limit to awun / nawun is
    to avoid races with other on flight commands which otherwise
    could cause unexpected results.
    
    This also means this debug module parameter feature is not supported
    if your drive does not support atomics / awun / nawun.
    
    Suggested-by: Dan Helmick <dan.helmick@samsung.com>
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -88,6 +88,10 @@ module_param(apst_secondary_latency_tol_us, ulong, 0644);
 MODULE_PARM_DESC(apst_secondary_latency_tol_us,
 	"secondary APST latency tolerance in us");
 
+static unsigned int debug_large_atomics;
+module_param(debug_large_atomics, uint, 0644);
+MODULE_PARM_DESC(debug_large_atomics, "allow large atomics <= awun or nawun <= mdts");
+
 /*
  * nvme_wq - hosts nvme related works that are not reset or delete
  * nvme_reset_wq - hosts nvme reset works
@@ -1931,6 +1935,20 @@ static void nvme_update_disk_info(struct gendisk *disk,
 		 * be aware of out of order reads/writes as npwg and nows
 		 * are purely performance optimizations.
 		 */
+
+		/*
+		 * If you're not concerned about power failure, in theory,
+		 * you should be able to experiment up to awun rather safely.
+		 *
+		 * Ignore qemu awun value of 1.
+		 */
+		if (debug_large_atomics && awun != 1) {
+			debug_large_atomics = min(awun_bs, debug_large_atomics);
+			phys_bs = atomic_bs = debug_large_atomics;
+			dev_info(ns->ctrl->device,
+				 "Forcing large atomic: %u (awun_bs: %u awun: %u)\n",
+				 debug_large_atomics, awun_bs, awun);
+		}
 	}
 
 	blk_queue_logical_block_size(disk->queue, bs);
