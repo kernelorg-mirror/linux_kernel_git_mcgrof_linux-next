e84ac7d725a090429ab0aa30d3cd30645fa536c0
Luis Chamberlain <mcgrof@kernel.org>
2023-06-15 23:27:14 -0700

    bdev: allow to switch between bdev aops
    Now that we have annotations for filesystems which require buffer-heads we
    can use that flag to verify if we can use the filesystem on the target
    block devices which require higher order folios. A filesystems which requires
    buffer-heads cannot be used on block devices which have a logical block size
    greater than PAGE_SIZE. We also want to allow to use buffer-head filesystems
    on block devices and at a later time then unmount and switch to a filesystem
    which supports bs > PAGE_SIZE, even if the logical block size of the block
    device is PAGE_SIZE, and this requires iomap. Provide helpers to do all these
    checks and resets the aops to iomap when needed.
    
    Leaving iomap in place after an umount would not make such block devices usable
    for buffer-head filesystems so we must reset the aops to buffer-heads also
    on unmount.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -1629,6 +1629,13 @@ void bdev_statx(struct inode *backing_inode, struct kstat *stat,
 		u32 request_mask);
 void printk_all_partitions(void);
 int __init early_lookup_bdev(const char *pathname, dev_t *dev);
+#ifdef CONFIG_BUFFER_HEAD
+void bdev_aops_reset(struct block_device *bdev);
+#else
+static inline void bdev_aops_reset(struct block_device *bdev)
+{
+}
+#endif
 #else
 static inline void invalidate_bdev(struct block_device *bdev)
 {
