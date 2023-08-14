6d093b2baf11571811bf882d664dbf62d38cc758
Luis Chamberlain <mcgrof@kernel.org>
2023-08-14 00:52:53 -0700

    filemap: verify folio index on do_async_mmap_readahead()
    do_async_mmap_readahead() is called with a folio, verify that
    the folio index matches the index if we round down the page
    fault offset.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- mm/filemap.c
+++ mm/filemap.c
@@ -3264,9 +3264,11 @@ static struct file *do_async_mmap_readahead(struct vm_fault *vmf,
 	int order = mapping_min_folio_order(mapping);
 	unsigned int nrpages = 1U << order;
 	struct file_ra_state *ra = &file->f_ra;
-	DEFINE_READAHEAD(ractl, file, ra, file->f_mapping, round_down(vmf->pgoff, nrpages));
+	DEFINE_READAHEAD(ractl, file, ra, file->f_mapping, folio->index);
 	struct file *fpin = NULL;
 	unsigned int mmap_miss;
+
+	WARN_ON_ONCE(round_down(vmf->pgoff, nrpages) != folio->index);
 
 	/* If we don't want any read-ahead, don't bother */
 	if (vmf->vma->vm_flags & VM_RAND_READ || !ra->ra_pages)
