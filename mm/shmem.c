de64e294bd14785ea421cbad1df8c43dac971080
Daniel Gomez <da.gomez@samsung.com>
2023-09-15 09:51:24 +0000

    shmem: drop BLOCKS_PER_PAGE macro
    The commit [1] replaced all BLOCKS_PER_PAGE in favor of the
    generic PAGE_SECTORS but definition was not removed. Drop it
    as unused macro.
    
    [1] e09764cff44b5 ("shmem: quota support").
    
    Signed-off-by: Daniel Gomez <da.gomez@samsung.com>
    Reviewed-by: Luis Chamberlain <mcgrof@kernel.org>

--- mm/shmem.c
+++ mm/shmem.c
@@ -84,7 +84,6 @@ static struct vfsmount *shm_mnt;
 
 #include "internal.h"
 
-#define BLOCKS_PER_PAGE  (PAGE_SIZE/512)
 #define VM_ACCT(size)    (PAGE_ALIGN(size) >> PAGE_SHIFT)
 
 /* Pretend that each entry is of this size in directory's i_size */
